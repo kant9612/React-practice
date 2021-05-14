66a85fac525b76f925bd40708b6562a1571eec71
kant9612 <kanstephentakayama@gmail.com>
2021-05-14 11:17:48 +0000

    Initial Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'; //reactをインポート
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello React</h1>
+    );
+  }
+}
+
+export default App; //Appにエクスポート
