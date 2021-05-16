398c38088dbb03909b61d1967ffac13d4ff52aa2
kant9612 <kanstephentakayama@gmail.com>
2021-05-16 00:09:26 +0000

    1-8

--- progate07/App.js
+++ progate07/App.js
@@ -7,10 +7,10 @@ class App extends React.Component {
     	  <h1>こんにちは、にんじゃわんこさん！</h1>
     	  {/* <button>タグ「ひつじ仙人」を追加してください */}
     	  <button>ひつじ仙人</button>
-    	  
+
     	  {/* <button>タグ「にんじゃわんこ」を追加してください */}
     	  <button>にんじゃわんこ</button>
-    	  
+
     	</div>
     );
   }
