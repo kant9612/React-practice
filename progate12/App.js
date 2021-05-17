598de4feaa4be0043afc22a3199c3c74cd3d14f4
kant9612 <kanstephentakayama@gmail.com>
2021-05-17 04:13:11 +0000

    1-13

--- progate12/App.js
+++ progate12/App.js
@@ -5,22 +5,22 @@ class App extends React.Component {
     super(props);
     this.state = {name: 'にんじゃわんこ'};
   }
-  
+
   // handleClickメソッドを定義してください
   handleClick(name){
     this.setState({name: name});
   }
-  
+
   render() {
     return (
     	<div>
     	  <h1>こんにちは、{this.state.name}さん！</h1>
     	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
         <button onClick={() => {this.handleClick('ひつじ仙人')}}>ひつじ仙人</button>
-        
+
     	  {/* onClickイベント内の処理を、handleClickメソッドを呼び出す処理に書き換えてください*/}
         <button onClick={() => {this.handleClick('にんじゃわんこ')}}>にんじゃわんこ</button>
-        
+
       </div>
     );
   }
